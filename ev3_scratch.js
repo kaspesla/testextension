ba5f6b6174073ab4bea460b8bb03347470662a35
kaspesla <ken@ecamm.com>
2015-10-08 12:42:33 -0400

    foo

--- ev3_scratch.js
+++ ev3_scratch.js
@@ -758,7 +758,7 @@ function playFreqM2M(freq, duration)
     lineCheckingInterval = window.setInterval(function()
     {
         readFromColorSensor(portInt, modeCode);
-         if (global_sensor_result[portInt] < 10 && global_sensor_result[portInt] >= 0)    // darkness or just not reflection (air)
+         if (global_sensor_result[portInt] < 25 && global_sensor_result[portInt] >= 0)    // darkness or just not reflection (air)
          {
                 clearInterval(lineCheckingInterval);
                 lineCheckingInterval = 0;
