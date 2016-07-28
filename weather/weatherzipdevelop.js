a3dc15c0bc6a527a953b8041eb391923d0162472
Maclean Parisi <ninjamurloc@gmail.com>
2016-07-28 15:19:04 -0400

    Tested use of an atom package that cleans up javascript
    things should probably be indented really well now

--- weather/weatherzipdevelop.js
+++ weather/weatherzipdevelop.js
@@ -1,18 +1,16 @@
-
-function timeStamp()
-{
-    return (new Date).toISOString().replace(/z|t/gi,' ').trim();
+function timeStamp() {
+    return (new Date).toISOString().replace(/z|t/gi, ' ').trim();
 }
 
-function console_log(str)
-{
-    console.log(timeStamp() + ": "  + str);
+function console_log(str) {
+    console.log(timeStamp() + ": " + str);
 }
 
 var cityid = "4929004";
 var citystring = "Amesbury, MA";
 var locationRequestType = "id";
 
+<<<<<<< Updated upstream
 var cityids =
 {
     "Amesbury, MA" : "4929004",
@@ -27,98 +25,111 @@ var cityids =
     "Buenos Aires, Argentina" : "3435910",
     "Suva, Fiji" : "2198148",
     "Lobujya, Nepal" : "1283119"
+=======
+var cityids = {
+    "Amesbury, MA": "4929004",
+    "New York, NY": "5128581",
+    "London, UK": "2643743",
+    "Paris, France": "2988507",
+    "Honolulu, Hawaii": "5856195",
+    "Antarctica": "6255152",
+    "Orlando, Florida": "4167147",
+    "Beijing, China": "1816670",
+    "Doha, Qatar": "290030",
+    "Buenos Aires, Argentina": "3435910",
+    "Fiji": "2198148",
+    "Mount Everest": "4517586"
+>>>>>>> Stashed changes
 };
 
 var cities = [];
-for(var k in cityids) cities.push(k);
+for (var k in cityids) cities.push(k);
 
 
-new (function(ext) {
-  // Cleanup function when the extension is unloaded
+new(function(ext) {
+    // Cleanup function when the extension is unloaded
 
-  ext._getStatus = function()
-  {
-    // xxx do a ping
-     return { status:2, msg:'Ready' };
-  };
-
-
-  ext._shutdown = function()
-  {
+    ext._getStatus = function() {
+        // xxx do a ping
+        return {
+            status: 2,
+            msg: 'Ready'
+        };
+    };
 
 
-  };
+    ext._shutdown = function() {
 
-     var cachedWeather = 0;
-     var currentWeather = 0;
-     var cacheInterval = 0;
 
-     function kelvinToCelsius(value)
-     {
+    };
+
+    var cachedWeather = 0;
+    var currentWeather = 0;
+    var cacheInterval = 0;
+
+    function kelvinToCelsius(value) {
         return value - 273.15;
-     };
+    };
 
 
-    function kelvinToFahrenheit(value)
-     {
+    function kelvinToFahrenheit(value) {
         return (kelvinToCelsius(value) * 1.8) + 32;
-     }
+    }
 
-     function sendRequest(command, callback)
-     {
-        if (cachedWeather)
-        {
+    function sendRequest(command, callback) {
+        if (cachedWeather) {
             if (callback)
                 callback();
-        }
-        else
-        {
+        } else {
 
             $.ajax({
                 type: "GET",
                 dataType: "jsonp",
                 url: "http://api.openweathermap.org/" + "data/2.5/weather?" + locationRequestType + "=" + cityid + "&APPID=bd9989ac922908fed9b1ec1521595d99",
                 success: function(data) {
+<<<<<<< Updated upstream
                    cachedWeather = data;
                    currentWeather = JSON.parse(JSON.stringify(data));
                    console.log("Got weather:"  + JSON.stringify(data));
                     {
+=======
+                    cachedWeather = data;
+                    currentWeather = JSON.parse(JSON.stringify(data));
+                    console_log("Got weather:" + JSON.stringify(data)); {
+>>>>>>> Stashed changes
                         if (callback)
                             callback();
                     }
                 },
                 error: function(jqxhr, textStatus, error) {
-                   console_log("error: " + error + " " + textStatus);
+                    console_log("error: " + error + " " + textStatus);
                     if (callback)
                         callback();
                 }
-                });
-            if (cacheInterval)
-     { clearTimeout(cacheInterval); }
+            });
+            if (cacheInterval) {
+                clearTimeout(cacheInterval);
+            }
             cacheInterval = window.setTimeout(function() {
-                                              cacheInterval = 0;
-                                              cachedWeather = 0;
-                                              console_log("clearing cache");
-                                       }, (60000 * 5)); // 5 minutes
+                cacheInterval = 0;
+                cachedWeather = 0;
+                console_log("clearing cache");
+            }, (60000 * 5)); // 5 minutes
         }
 
-     }
+    }
 
-     function changeLocation(dir, callback)
-     {
+    function changeLocation(dir, callback) {
         var x = 0;
-        for(x in cities)
-        {
-            if (cities[x] == citystring)
-            {
+        for (x in cities) {
+            if (cities[x] == citystring) {
                 break;
             }
         }
         x = parseInt(x);
         x = x + dir;
         var n = cities.length;
-        if (x < 0)
-        {
+        if (x < 0) {
             x = n - 1;
         }
         x %= n;
@@ -126,56 +137,50 @@ new (function(ext) {
         var newcit = cities[x];
 
         setTheLocation(newcit, callback);
-     }
+    }
 
 
-     function setTheLocation(location, callback)
-     {
-         citystring = location;
-         var loc = cityids[location];
-         console_log("Set location for " + location + " to " + loc);
-         cityid = loc;
-         cachedWeather = 0;  // clear cache
-         locationRequestType = "id";
+    function setTheLocation(location, callback) {
+        citystring = location;
+        var loc = cityids[location];
+        console_log("Set location for " + location + " to " + loc);
+        cityid = loc;
+        cachedWeather = 0; // clear cache
+        locationRequestType = "id";
 
-        sendRequest("",callback);
-     }
+        sendRequest("", callback);
+    }
 
-     function setLocationZipcode(zip, callback)
-     {
-         console_log("Set zipcode to " + zip);
-         cityid = zip;
-         cachedWeather = 0;  // clear cache
-         locationRequestType = "zip";
+    function setLocationZipcode(zip, callback) {
+        console_log("Set zipcode to " + zip);
+        cityid = zip;
+        cachedWeather = 0; // clear cache
+        locationRequestType = "zip";
 
-        sendRequest("",callback);
-     }
+        sendRequest("", callback);
+    }
 
-     ext.updateWeather = function(callback)
-     {
-        sendRequest("",callback);
-     }
+    ext.updateWeather = function(callback) {
+        sendRequest("", callback);
+    }
 
-     ext.getTemp= function()
-     {
-     if ( !currentWeather )
-     {
-      return "";
-     }
+    ext.getTemp = function() {
+        if (!currentWeather) {
+            return "";
+        }
         var degreesF = kelvinToFahrenheit(currentWeather.main.temp);
-        degreesF = Math.round(10*degreesF)/10;
+        degreesF = Math.round(10 * degreesF) / 10;
         return degreesF;
-     };
+    };
 
-     ext.getTempC= function()
-     {
-     if ( !currentWeather )
-     {
-      return "";
-     }
+    ext.getTempC = function() {
+        if (!currentWeather) {
+            return "";
+        }
         var degreesC = kelvinToCelsius(currentWeather.main.temp);
-        degreesC = Math.round(10*degreesC)/10;
+        degreesC = Math.round(10 * degreesC) / 10;
         return degreesC;
+<<<<<<< Updated upstream
      };
 
      ext.getWeather= function()
@@ -209,28 +214,51 @@ if (isNaN(w))
 
      ext.setLocation = function(location, callback)
      {
+=======
+    };
+
+    ext.getWeather = function() {
+        if (!currentWeather) {
+            return "";
+        }
+        return currentWeather.weather[0].main;
+    };
+
+    ext.getWeatherDetails = function() {
+        if (!currentWeather) {
+            return "";
+        }
+        return currentWeather.weather[0].description;
+    };
+
+    ext.getWindDirection = function() {
+        if (!currentWeather) {
+            return "";
+        }
+        return currentWeather.wind.deg;
+    };
+
+    ext.setLocation = function(location, callback) {
+>>>>>>> Stashed changes
         setTheLocation(location, callback);
-     }
+    }
 
-     ext.setZipcodeLocation = function(zip, callback)
-     {
+    ext.setZipcodeLocation = function(zip, callback) {
         setLocationZipcode(zip, callback);
-     }
+    }
 
-     ext.currentLocation = function()
-     {
+    ext.currentLocation = function() {
         return currentWeather.name;
-     }
+    }
 
 
-     ext.nextLocation = function(callback)
-     {
+    ext.nextLocation = function(callback) {
         changeLocation(1, callback);
-     }
+    }
 
-     ext.prevLocation = function(callback)
-     {
+    ext.prevLocation = function(callback) {
         changeLocation(-1, callback);
+<<<<<<< Updated upstream
      }
 
   // Block and block menu descriptions
@@ -259,4 +287,34 @@ if (isNaN(w))
   ScratchExtensions.register('Weather', descriptor2, ext);
   console.log('registered: ');
                  });
+=======
+    }
+
+    // Block and block menu descriptions
+    var descriptor2 = {
+        blocks: [
+            ['w', 'update weather', 'updateWeather'],
+            ['r', 'current temperature in Fahrenheit', 'getTemp'],
+            ['r', 'current temperature in Celsius', 'getTempC'],
+            ['r', 'current weather type', 'getWeather'],
+            ['r', 'current weather details', 'getWeatherDetails'],
+            ['r', 'current wind direction', 'getWindDirection'],
+            ['-'],
+            ['w', 'set location to %m.locations', 'setLocation', "Amesbury, MA"],
+            ['w', 'set zipcode to %s', 'setZipcodeLocation', "01860"],
+            ['r', 'current location', 'currentLocation'],
+            ['w', 'next location', 'nextLocation'],
+            ['w', 'previous location', 'prevLocation'],
+
+        ],
+        menus: {
+            "locations": cities
+
+        },
+    };
+    sendRequest("", function() {
+        ScratchExtensions.register('Weather', descriptor2, ext);
+        console.log('registered: ');
+    });
+>>>>>>> Stashed changes
 })({});
