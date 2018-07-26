2fc0010db8ca7fc14136e71480bd00c23edc335a
Macman1223 <ninjamurloc@gmail.com>
2018-07-26 10:37:46 -0400

    added fade

--- hue_lights/hue_lights_cookies.js
+++ hue_lights/hue_lights_cookies.js
@@ -190,6 +190,7 @@ ext.lightColorHex = function(light, hex, callback)
      sendLightColorCommand(light, rgb2hsv(hexToRgb(hex)), 0);
      waitAndCall(callback, noWait);
 }
+
      
 function fadeMap(fade)
  {
@@ -218,6 +219,13 @@ ext.lightColorRGBFade = function(light,r,g,b, fade, callback)
      sendLightColorCommand(light, rgb2hsv(colorValues[color]), fad);
      waitAndCall(callback, fade);
  }
+
+ ext.lightColorHex = function(light, hex, fade, callback)
+{
+    fad = fadeMap(fade)
+     sendLightColorCommand(light, rgb2hsv(hexToRgb(hex)), fad);
+     waitAndCall(callback, noWait);
+}
 
  ext.lightOn = function(light, callback)
  {
@@ -381,6 +389,7 @@ function registerExtension()
            ['w', '%m.lights r: %n g: %n b: %n',                                   'lightColorRGB',     name1,  "255", "0", "255"],
            ['w', '%m.lights r: %n g: %n b: %n fade: %n seconds',                                   'lightColorRGBFade',     name1,  "255", "0", "255", "1.0"],
            ['w', '%m.lights hex: %s',                                   'lightColorHex',     name1,  "#FF0000"],
+           ['w', '%m.lights hex: %s fade: %n seconds',                                   'lightColorHexFade',     name1,  "#FF0000", "1.0"],
            ["-"],
            ["-"],
            [' ', 'unpair base station','unpair']
